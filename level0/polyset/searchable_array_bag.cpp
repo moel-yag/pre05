cb90bca70f7af6889a45f533e3aa74e2f6e97450
medel <mohammedelyagoubi51@gmail.com>
2026-07-12 13:32:39 +0100

    *

--- level0/polyset/searchable_array_bag.cpp
+++ level0/polyset/searchable_array_bag.cpp
@@ -1,35 +1,36 @@
 #include "searchable_array_bag.hpp"
 
-searchable_array_bag::searchable_array_bag() : array_bag() {};
 
-searchable_array_bag::searchable_array_bag(const searchable_array_bag &copy) : array_bag(copy) {};
-
-searchable_array_bag &searchable_array_bag::operator=(const searchable_array_bag &other)
+searchable_array_bag::searchable_array_bag()
 {
-    if (this != &other)
-    {
-        array_bag::operator=(other);
-    }
 
-    return *this;
 }
 
-searchable_array_bag::~searchable_array_bag(){};
-
-bool searchable_array_bag::has(int x) const
+searchable_array_bag::searchable_array_bag(const searchable_array_bag& source) : array_bag(source)
 {
-    if (this->size <= 0)
-        return false;
-    
-    int i = 0;
-    
-    while(i < this->size)
-    {
-        if (this->data[i] == x)
-            return true;
-        i++;
-    }
-    return false;
+
 }
 
-    
+searchable_array_bag& searchable_array_bag::operator=(const searchable_array_bag& source)
+{
+	if(this != &source)
+	{
+		array_bag::operator=(source);
+	}
+	return(*this);
+}
+
+bool searchable_array_bag::has(int value) const
+{
+	for(int i = 0; i < this->size; i++)
+	{
+		if(this->data[i] == value)
+			return (true);
+	}
+	return(false);
+}
+
+searchable_array_bag::~searchable_array_bag()
+{
+
+}
