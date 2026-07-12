de01ebdc4170903f4fcf0c1dc6a2a960a430dcb6
medel <mohammedelyagoubi51@gmail.com>
2026-07-12 13:33:13 +0100

    Update searchable_array_bag.hpp

--- level0/polyset/searchable_array_bag.hpp
+++ level0/polyset/searchable_array_bag.hpp
@@ -1,19 +1,15 @@
-#ifndef SEARCHABLE_ARRAY_BAG_HPP
-#define SEARCHABLE_ARRAY_BAG_HPP
+#pragma once
 
 #include "array_bag.hpp"
 #include "searchable_bag.hpp"
 
+
 class searchable_array_bag : public array_bag, public searchable_bag
 {
-    public:
-        searchable_array_bag();
-        searchable_array_bag(const searchable_array_bag &copy);
-        searchable_array_bag &operator=(const searchable_array_bag &other);
-        virtual ~searchable_array_bag();
-
-        virtual bool has(int x) const;
-        
+	public:
+		searchable_array_bag();
+		searchable_array_bag(const searchable_array_bag& source);
+		searchable_array_bag& operator=(const searchable_array_bag& source);
+		bool has(int) const;
+		~searchable_array_bag();
 };
-
-#endif
