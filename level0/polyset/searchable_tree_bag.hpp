e2888669b379ed3ce07263fc8b6870a3715e08ca
medel <mohammedelyagoubi51@gmail.com>
2026-07-12 13:34:00 +0100

    Update searchable_tree_bag.hpp

--- level0/polyset/searchable_tree_bag.hpp
+++ level0/polyset/searchable_tree_bag.hpp
@@ -1,19 +1,17 @@
-#ifndef SEARCHABLE_TREE_BAG_HPP
-#define SEARCHABLE_TREE_BAG_HPP
+#pragma once
 
 #include "tree_bag.hpp"
 #include "searchable_bag.hpp"
 
+
 class searchable_tree_bag : public tree_bag, public searchable_bag
 {
-    public: 
-        searchable_tree_bag();
-        searchable_tree_bag(const searchable_tree_bag &copy);
-        searchable_tree_bag &operator=(const searchable_tree_bag &other);
-        virtual ~searchable_tree_bag();
-
-        virtual bool has(int x) const;
-
+	private:
+		bool search(node* node, const int value) const;
+	public:
+		searchable_tree_bag();
+		searchable_tree_bag(const searchable_tree_bag& source);
+		searchable_tree_bag& operator=(const searchable_tree_bag& source);
+		bool has(int) const;
+		~searchable_tree_bag();
 };
-
-#endif
