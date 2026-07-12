e3fdc2ffbedeecd39abc7c91886afad19c9c5e19
medel <mohammedelyagoubi51@gmail.com>
2026-07-12 13:33:35 +0100

    Refactor searchable_tree_bag methods and constructors

--- level0/polyset/searchable_tree_bag.cpp
+++ level0/polyset/searchable_tree_bag.cpp
@@ -1,33 +1,45 @@
 #include "searchable_tree_bag.hpp"
 
-searchable_tree_bag::searchable_tree_bag() : tree_bag(){};
 
-searchable_tree_bag::searchable_tree_bag(const searchable_tree_bag &copy) : tree_bag(copy){};
-
-searchable_tree_bag &searchable_tree_bag::operator=(const searchable_tree_bag &other)
+searchable_tree_bag::searchable_tree_bag()
 {
-    if (this != &other)
-    {
-        tree_bag::operator=(other);
-    }
-    return *this;
+
 }
 
-searchable_tree_bag::~searchable_tree_bag(){};
-
-bool searchable_tree_bag::has(int x) const
+searchable_tree_bag::searchable_tree_bag(const searchable_tree_bag& source) : tree_bag(source)
 {
-    const node *cur = this->tree;
 
-    while (cur != 0)
-    {
-        if (x == cur->value)
-            return true;
+}
 
-        if (x < cur->value)
-            cur = cur->l;
-        else
-            cur = cur->r;
-    }
-    return false;
+searchable_tree_bag& searchable_tree_bag::operator=(const searchable_tree_bag& source)
+{
+	if(this != &source)
+	{
+		tree_bag::operator=(source);
+	}
+	return(*this);
+}
+
+bool searchable_tree_bag::search(node* node, const int value) const
+{
+	if(node == nullptr)
+		return(false);
+	if(node->value == value)
+		return(true);
+	else if(value < node->value)
+	{
+		return(search(node->l, value));
+	}
+	else
+		return(search(node->r, value));
+}
+
+bool searchable_tree_bag::has(int value) const
+{
+	return(search(this->tree, value));
+}
+
+searchable_tree_bag::~searchable_tree_bag()
+{
+
 }
