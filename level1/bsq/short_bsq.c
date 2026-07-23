86311456657c87faef5a6cc781818bcb0cd78d0d
medel <mohammedelyagoubi51@gmail.com>
2026-07-23 18:26:38 +0100

    Update and rename short_BSQ.c to short_bsq.c

--- level1/bsq/short_bsq.c
+++ level1/bsq/short_bsq.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <errno.h>
 
 int min(int a, int b) { return a < b ? a : b; }
 
-void solve(FILE *f) {
-    int n, w = 0, i, j, s = 0, x = 0, y = 0, *d;
-    char e, o, p, b[4096], **g;
+int slen(char *s)
+{
+    int i = 0;
+    while (s[i] && s[i] != '\n') i++;
+    return i;
+}
 
-    if (!fgets(b, 4096, f)) goto err;
-    n = atoi(b);
-    i = strlen(b);
-    if (n <= 0 || i < 4 || (e = b[i - 4]) == (o = b[i - 3]) || e == (p = b[i - 2]) || o == p) goto err;
+void solve(FILE *f)
+{
+    char *line = 0, **g = 0;
+    size_t l = 0;
+    int n, w = 0, i, j, s = 0, x = 0, y = 0, *d = 0;
+    char e, o, p;
+
+    if (fscanf(f, "%d %c %c %c", &n, &e, &o, &p) != 4 || n <= 0 || e == o || e == p || o == p) goto err;
+    if (getline(&line, &l, f) <= 0) goto err;
 
     g = malloc(n * sizeof(char *));
     for (i = 0; i < n; i++) {
-        if (!fgets(b, 4096, f)) goto err;
-        if ((j = strlen(b) - 1) != w && w) goto err;
+        if (getline(&line, &l, f) <= 0) goto err;
+        if ((j = slen(line)) != w && w) goto err;
         if (!w) w = j;
         if (w <= 0) goto err;
-        g[i] = strdup(b);
+        g[i] = line;
+        line = 0; l = 0;
         for (j = 0; j < w; j++)
             if (g[i][j] != e && g[i][j] != o) goto err;
     }
@@ -34,15 +43,23 @@ void solve(FILE *f) {
 
     for (i = 0; i < n; i++) {
         for (j = 0; j < w; j++)
-            putchar(i >= y && i < y + s && j >= x && j < x + s ? p : g[i][j]);
-        putchar('\n');
+            g[i][j] = (i >= y && i < y + s && j >= x && j < x + s) ? p : g[i][j];
+        g[i][w] = '\n';
+        g[i][w + 1] = 0;
+        fputs(g[i], stdout);
     }
+
+    for (i = 0; i < n; i++) free(g[i]);
+    free(g); free(d); free(line);
     return;
 err:
     fprintf(stderr, "map error\n");
+    if (g) for (i = 0; i < n && g[i]; i++) free(g[i]);
+    free(g); free(d); free(line);
 }
 
-int main(int c, char **v) {
+int main(int c, char **v)
+{
     if (c == 1) solve(stdin);
     else for (int k = 1; k < c; k++) {
         FILE *f = fopen(v[k], "r");
